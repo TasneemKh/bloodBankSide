92a745f7acd8eaf537069b5e4cf8cb99e583d766
Tasneem Khrais <eng.khraistasneem@gmail.com>
2020-08-02 03:58:09 +0300

    "v2 design side"

---
+++ app/src/main/java/com/example/bloodbank/bloodBankReq.java
@@ -0,0 +1,14 @@
+package com.example.bloodbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bloodBankReq extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blood_bank_req);
+    }
+}
